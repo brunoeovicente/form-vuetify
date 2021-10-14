c27af5eef164d72c45be01c8db5f7d8c26fccfb9
unknown <brunoeovicente@gmail.com>
2021-10-13 21:07:11 -0300

    Form template

---
+++ src/App.vue
@@ -0,0 +1,81 @@
+<template>
+  <v-app id="inspire">
+    <v-app-bar app>
+      <div class="d-flex aling-center justify-center">
+        <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+        <h3 class="my-auto">Inscrição</h3>
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-dots-vertical</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <v-container>
+        <v-form>
+          <v-row class="mt-0 pt-0">
+            <v-col cols="12" sm="6">
+              <v-text-field
+                label="Nome"
+                placeholder=""
+                filled
+                v-model="formData.name"
+              ></v-text-field>
+            </v-col>
+            <v-col cols="12" sm="6">
+              <v-text-field
+                label="Sobrenome"
+                placeholder=""
+                filled
+                v-model="formData.surname"
+              ></v-text-field>
+            </v-col>
+          </v-row>
+          <!--  -->
+          <v-row class="mt-0 pt-0">
+            <v-col cols="12" sm="6">
+              <v-select
+                :items="items"
+                label="Sexo"
+                filled
+                v-model="formData.gender"
+              ></v-select>
+            </v-col>
+            <v-col cols="12" sm="6">
+              <v-text-field
+                label="E-mail"
+                placeholder="@gmail.com"
+                filled
+                v-model="formData.email"
+              ></v-text-field>
+            </v-col>
+          </v-row>
+          <!--  -->
+          [{{ formData }}]
+          <v-row class="mt-0">
+            <v-col>
+              <v-btn class="d-flex ml-auto" color="success">Enviar</v-btn>
+            </v-col>
+          </v-row>
+        </v-form>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: "App",
+
+  components: {},
+
+  data: () => ({
+    formData: { name: "", surname: "", gender: "", email: "" },
+    items: ["Masculino", "Feminino"],
+  }),
+};
+</script>
